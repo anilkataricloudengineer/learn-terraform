3fb3a723dea83883685a7ca39ba60b14f627db41
Anil Katari <anilkataricloudengineer@gmail.com>
2025-10-08 23:08:11 +0530

    Roboshop Project

--- 03-resource/data.tf
+++ 03-resource/data.tf
@@ -5,5 +5,5 @@ data "azurerm_resource_group" "example" {
 data "azurerm_subnet" "example" {
   name                 = "default"
   virtual_network_name = "project-setup-network"
-  resource_group_name  = "data.azurerm_resource_group.example.name"
+  resource_group_name  = data.azurerm_resource_group.example.name
 }
