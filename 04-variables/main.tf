f8a1ebb95ddc47ea9b2cc51cec8e7f4d0f9474b2
Anil Katari <anilkataricloudengineer@gmail.com>
2025-10-08 23:42:08 +0530

    Roboshop Project

---
+++ 04-variables/main.tf
@@ -0,0 +1,54 @@
+variable "s1" {
+  default = "Hello"
+}
+
+variable "n1" {
+  default = 2
+}
+
+variable "b1" {
+  default = true
+}
+
+# Data types that terraform supports
+# 1. string
+# 2. Number
+# 3. Boolean
+
+# String only require quotes, that to be only double quotes. Terraform does not have single quotes support.
+
+variable "v1" {
+  default = "Hello"
+}
+
+variable "v2" {
+  default = [
+  "Hello",
+    2,
+    false
+  ]
+}
+
+variable "v3" {
+  default = {
+    course = "devops"
+    cloud = "azure"
+  }
+}
+
+# Variable type that terraform supports
+# 1. Plain
+# 2. List ( A key having multiple values , And each value can be different data type)
+# 3. Map (A key is having again keys and values)
+
+## Access your variable
+
+output "v1" {
+  value = var.v1
+}
+
+# while accessing the variable if we have some string combination then we can access it with ${}
+
+output "v2" {
+  value = "${var.v1} - john"
+}
