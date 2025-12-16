e5de14a45f0f1bf22c448252a3fbe9f48080c8a3
Anil Katari <anilkataricloudengineer@gmail.com>
2025-12-16 20:56:14 +0530

    Roboshop Project

---
+++ 10-remote-state/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "project-steup-1"
+    storage_account_name = "katari"
+    container_name       = "tfstates"
+    key                  = "example.tfstate"
+  }
+}
+
+output "test" {
+  value = "Hello"
+}
