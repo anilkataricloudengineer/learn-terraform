98175369414c29ccb7731d8e1a9e04cb03879104
Anil Katari <anilkataricloudengineer@gmail.com>
2025-12-16 15:30:56 +0530

    Roboshop Project

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -3,3 +3,8 @@ variable "a" {}
 output "test" {
   value = var.a > 10 ? "a is greater than 10" : " a is less than 10"
 }
+# these conditions with combination of loops can be used to determine whether a resource to run or not.
+
+
+
+
