504c86d9ad8d112a773d4509a592ba0e385e8ebb
Anil Katari <anilkataricloudengineer@gmail.com>
2025-12-17 23:10:12 +0530

    Roboshop Project

--- 11-vault/main.tf
+++ 11-vault/main.tf
@@ -17,5 +17,5 @@ data "vault_kv_secret" "secret_data" {
 
 resource "local_file" "local" {
   filename = "/tmp/pass"
-  content = data.vault_kv_secret.secret_data
+  content = data.vault_kv_secret.secret_dataa
 }
